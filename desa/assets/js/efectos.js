d63df21570da23f38ea6299bdec4c2c2d6313288
spicke23 <angelo.valenzuela@alumnos.uta.cl>
2021-11-14 00:18:41 -0300

    TrabajoPers_javascript
    Se agrega directorio JS
    Se agrega archivo efectos.js
    Se modifica index.html para visualizar efecto JS.

---
+++ desa/assets/js/efectos.js
@@ -0,0 +1,8 @@
+var cantidad = 1
+var MuestraVentanaAlerta = function (){
+    if(cantidad){
+        let mensaje = 'BienVenido a tu sitio de comida saludable'
+        alert(mensaje)
+    }
+    cantidad = 0
+}
