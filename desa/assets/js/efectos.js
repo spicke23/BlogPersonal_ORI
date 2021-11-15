f99e7083ad52559ca08a2734cfcb85ca3cb34b9a
spicke23 <angelo.valenzuela@alumnos.uta.cl>
2021-11-15 10:25:59 -0300

    Tarea JavaScript
    Se modifica: index.html
    Se crea directorio ./assets/js
    Se crea archivo efectos.js dentro de directorio js
    Se modifica respuesta en Ejercicio_JavaScript.txt

--- desa/assets/js/efectos.js
+++ desa/assets/js/efectos.js
@@ -1,8 +1,2 @@
-var cantidad = 1
-function MuestraVentanaAlerta(){
-    if(cantidad){
-        let mensaje = 'BienVenido a tu sitio de comida saludable'
-        alert(mensaje)
-    }
-    cantidad = 0
-}
+var mensaje = 'BienVenido a tu sitio de comida saludable'
+alert(mensaje)
