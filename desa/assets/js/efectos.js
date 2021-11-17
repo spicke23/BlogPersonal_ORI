eaa48f7655ab8abd61251a7ea0a827ed82864cdc
spicke23 <angelo.valenzuela@alumnos.uta.cl>
2021-11-16 23:31:44 -0300

    Ejercicio Individual JavaScript II
    Modificación index.html
    Creación de función generarNuevoColor().

--- desa/assets/js/efectos.js
+++ desa/assets/js/efectos.js
@@ -1,2 +1,19 @@
-var mensaje = 'BienVenido a tu sitio de comida saludable'
-alert(mensaje)
+function MuestraVentana() {
+    var mensaje = 'BienVenido a tu sitio de comida saludable'
+    alert(mensaje)
+}
+
+window.onload = MuestraVentana
+
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+    let division = document.getElementById('comidas')
+	division.style.background = color;
+}
