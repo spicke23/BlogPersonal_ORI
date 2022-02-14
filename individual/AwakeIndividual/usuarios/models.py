30443fec39fa53f264074cff2f13e8603573949c
spicke23 <angelo.valenzuela@alumnos.uta.cl>
2022-02-14 10:44:16 -0300

    hola
    actualizacion de repo
    14-feb-22

--- individual/AwakeIndividual/usuarios/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Usuario(models.Model):
-    rut = models.CharField(max_length=10, null=False)
-    nombre = models.CharField(max_length=50, null=False)
-    apellido = models.CharField(max_length=50, null=False)
-    email = models.EmailField(max_length=50)
-    edad = models.IntegerField()
